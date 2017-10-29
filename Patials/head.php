7770f34bfdf60c22bde37e1b873d122464803da3
rosalieper <persiderosalie@gmail.com>
2017-10-29 21:55:58 +0100

    added more_vert icon action on the nav for more options

--- Patials/head.php
+++ Patials/head.php
@@ -1,10 +1,13 @@
 <?php
 
-echo "<nav><ul>
+echo "
+	  <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>
+	<nav><ul>
 		<li><a href = \"../index.php\">Home</a></li>
 		<li><a href = \"/Hobie-project/Patials/contact.php\">Contact</a></li>
 		<li><a href = \"/Hobie-project/Patials/signup.php\">SignUp</a></li>
 		<li><a href = \"/Hobie-project/Patials/login.php\">login</a></li>
+		<li><i class='large material-icons'>more_vert</i></li>
 		</ul>
 		</nav>";
 
